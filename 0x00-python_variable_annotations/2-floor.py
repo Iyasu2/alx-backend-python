8225c173d0738396f63b3e37db80261b15ddd531
iyasu2 <iyasuforreal@gmail.com>
2024-01-11 18:48:03 +0300

    s

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+this module contains the floor function
+'''
+
+
+import math
+def floor(n: float) -> int:
+    '''
+    this is the floor function
+    '''
+    return math.floor(n)
