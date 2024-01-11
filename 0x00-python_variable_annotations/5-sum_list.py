beb3c2eb182101254ce7989af438d045bc8da4d7
iyasu2 <iyasuforreal@gmail.com>
2024-01-11 19:01:16 +0300

    s

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''
+this module contains the sum_list function
+'''
+from typing import List
+
+
+def sum_float(input_list: List[float]) -> float:
+    '''
+    this is the sum_list function
+    '''
+    return sum(input_list)
