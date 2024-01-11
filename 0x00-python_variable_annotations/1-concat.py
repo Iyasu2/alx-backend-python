64ed72d37b86bdd1e0ec2f304c12ed24c403fd3c
iyasu2 <iyasuforreal@gmail.com>
2024-01-11 18:40:14 +0300

    s

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+'''
+this module contains the concat function
+'''
+
+
+def concat(str1: str, str2: str) -> str:
+    '''
+    this is the concat function
+    '''
+    return str1 + str2
