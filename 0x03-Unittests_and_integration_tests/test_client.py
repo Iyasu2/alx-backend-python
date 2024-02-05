88e2ad3474edd0da353b32894c99982ec9e86d32
iyasu2 <iyasuforreal@gmail.com>
2024-02-05 19:28:35 +0300

    s

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -24,3 +24,12 @@ class TestGithubOrgClient(unittest.TestCase):
         test_client = GithubOrgClient(org_name)
         self.assertEqual(test_client.org, {"payload": True})
         mock_get_json.assert_called_once_with(f"https://api.github.com/orgs/{org_name}")
+    @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
+    def test_public_repos_url(self, mock_org):
+        """Test that the result of _public_repos_url is the
+        expected one based on the mocked payload"""
+        payload = {"repos_url": "mocked_url"}
+        mock_org.return_value = payload
+
+        test_client = GithubOrgClient("org_name")
+        self.assertEqual(test_client._public_repos_url, "mocked_url")
