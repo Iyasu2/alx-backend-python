85475b42e0a49cad973fc29dcab1d67ce860a1f5
iyasu2 <iyasuforreal@gmail.com>
2024-02-05 18:51:17 +0300

    s

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -63,6 +63,7 @@ class TestGetJson(unittest.TestCase):
             self.assertEqual(get_json(test_url), test_payload)
             req_get.assert_called_once_with(test_url)
 
+
 class TestMemoize(unittest.TestCase):
     '''
     Tests the `memoize` function
