56225b675585d659457afe2e5dfbdcff0acf60fb
iyasu2 <iyasuforreal@gmail.com>
2024-02-05 19:42:53 +0300

    s

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -18,7 +18,7 @@ class TestGithubOrgClient(unittest.TestCase):
         ("abc",)
     ])
     @patch('client.get_json', return_value={"payload": True})
-    def test_org(self, org_name, mock_get_json):
+    def test_org(self, org_name: str, mock_get_json: Mock) -> None:
         '''
         this is the function
         '''
@@ -28,7 +28,7 @@ class TestGithubOrgClient(unittest.TestCase):
                 f"https://api.github.com/orgs/{org_name}")
 
     @patch.object(GithubOrgClient, 'org', new_callable=PropertyMock)
-    def test_public_repos_url(self, mock_org):
+    def test_public_repos_url(self, mock_org: PropertyMock) -> None:
         """Test that the result of _public_repos_url is the
         expected one based on the mocked payload"""
         payload = {"repos_url": "mocked_url"}
@@ -39,7 +39,7 @@ class TestGithubOrgClient(unittest.TestCase):
 
     @patch('client.get_json', return_value=[
         {"name": "repo1"}, {"name": "repo2"}])
-    def test_public_repos(self, mock_get_json):
+    def test_public_repos(self, mock_get_json: Mock) -> None:
         """Test that the list of repos is what you
         expect from the chosen payload"""
         with patch.object(
@@ -52,3 +52,16 @@ class TestGithubOrgClient(unittest.TestCase):
             self.assertEqual(test_client.public_repos(), ["repo1", "repo2"])
             mock_get_json.assert_called_once_with("mocked_url")
             mock_public_repos_url.assert_called_once()
+
+    @parameterized.expand([
+        ({"license": {"key": "my_license"}}, "my_license", True),
+        ({"license": {"key": "other_license"}}, "my_license", False)
+    ])
+    def test_has_license(
+            self,
+            repo: Dict[str, Dict[str, str]],
+            license_key: str,
+            expected: bool) -> None:
+        """Test that the has_license method returns the expected value"""
+        test_client = GithubOrgClient("org_name")
+        self.assertEqual(test_client.has_license(repo, license_key), expected)
